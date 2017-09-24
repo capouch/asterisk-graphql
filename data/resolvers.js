0857207e1c6d0e0f7345b6715604e2879e5bfea5
Brian Capouch <brianc@whipsaw.palaver.net>
2017-09-24 00:37:32 -0400

    Clean up formatting, add comments

--- data/resolvers.js
+++ data/resolvers.js
@@ -2,45 +2,51 @@ let ari = require('ari-client')
 
 const resolvers = {
   Query: {
+
+  // Get all available sounds
   sounds: () => {
+
+    // Connect to Asterisk ARI interface
     return ari.connect('http://knuckle.palaver.net:8088', 'brianc', 'getmeoutofhere')
     .then (function (client) {
-        return client.sounds.list()
-        .then (function (_sounds) {
-          // console.log('Got some sounds ' + _sounds)
-          return _sounds
-        })
-        .catch(function (err) {
-          console.log('Error in sounds query ' + err);
-          return err
-        })
+
+      // Use client handle to fetch list of sounds
+      return client.sounds.list()
+      .then (function (_sounds) {
+        return _sounds
+      })
+      .catch(function (err) {
+        console.log('Error in sounds query ' + err);
+        return err
+      })
     })
     .catch(function(err) {
-      console.log(err)
+      console.log('Error connecting: ' + err)
     })
   },
 
+  // Fetch a single sound from the ARI interface
   getSound: (_, { id } ) => {
     return ari.connect('http://knuckle.palaver.net:8088', 'brianc', 'getmeoutofhere')
-      .then (function(client) {
-          return client.sounds.get({ soundId: id })
-          .then (function(_sound) {
-            console.log('So far, so good: ' + _sound.id + " " + _sound.text + " " + JSON.stringify(_sound.formats))
-            return _sound
-          })
-          .catch(function(err){
-            console.log('No sounds currently :-(' + err)
-          })
-        })
+    .then (function(client) {
+      return client.sounds.get({ soundId: id })
+      .then (function(_sound) {
+        // console.log('So far, so good: ' + _sound.id + " " + _sound.text + " " + JSON.stringify(_sound.formats))
+        return _sound
+      })
       .catch(function(err){
-        console.log('Error ' + err)
+        console.log('No sounds currently :-(' + err)
+        })
+      })
+    .catch(function(err){
+      console.log('Error connecting ' + err)
       })
     },
   },
 
+  // Get format fields for a given sound
   Sound: {
     formats(sound) {
-      console.log('In Sound resolver')
       return sound.formats
       },
   },
